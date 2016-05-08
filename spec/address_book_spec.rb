cdadca83086abb0dbba342490b25f0eab4548fa9
Jocelyn Hsu <hsu.joce@gmail.com>
2016-05-08 01:25:30 +0000

    Assignment 21 completed

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -87,4 +87,35 @@ RSpec.describe AddressBook do
         end
     end
     
+    describe "#import_from_csv2" do
+        it "imports the correct number of entries from entries_2.csv" do
+            book.import_from_csv("entries_2.csv")
+            book_size = book.entries.size
+            
+            expect(book_size).to eq 3
+        end
+        
+        it "imports the 1st entry" do
+            book.import_from_csv("entries_2.csv")
+            # Check the first entry
+            entry_one = book.entries[1]
+            
+            check_entry(entry_one, "Jocelyn", "408-666-9836", "hsu.joce@gmail.com")
+        end
+        
+        it "imports the 2nd entry" do
+            book.import_from_csv("entries_2.csv")
+            # Check the second entry
+            entry_seven = book.entries[2]
+            check_entry(entry_seven, "Rene", "408-823-1702", "rlamador@gmail.com")
+        end
+        
+        it "imports the 3rd entry" do
+            book.import_from_csv("entries_2.csv")
+            # Check the third entry
+            entry_eight = book.entries[0]
+            check_entry(entry_eight, "FFTM", "323-850-8025", "contact@foodforthemoon.com")
+        end 
+    end
+
 end
